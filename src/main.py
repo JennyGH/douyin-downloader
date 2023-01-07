3712501b1aeab25725750e92a7b30cef4744ff50
JennyGH <674886730@qq.com>
2023-01-07 23:16:49 +0800

    更新远程接口调用

--- src/main.py
+++ src/main.py
@@ -32,12 +32,16 @@ def _make_fail_result(errmsg):
     return _make_result(-1, errmsg)
 
 
-def _get_douyin_video_url(share_url):
-    # https://v.douyin.com/LYDLoga/
+def _get_video_id(share_url):
     response = requests.get(url=share_url, headers=headers)
-    item_ids = os.path.basename(os.path.dirname(response.url))
-    log_debug(f'item_ids: {item_ids}')
-    api_url = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={item_ids}'
+    video_id = os.path.basename(os.path.dirname(response.url))
+    return video_id
+
+
+def _get_video_real_url_by_id_v1(video_id):
+    # https://v.douyin.com/LYDLoga/
+    log_debug(f'video_id: {video_id}')
+    api_url = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}'
     log_debug(f'api_url: {api_url}')
     response = requests.get(url=api_url, headers=headers)
     text = response.content.decode('utf-8')
@@ -65,6 +69,7 @@ def _get_douyin_video_url(share_url):
             log_error(
                 f'No `download_addr` or `play_addr` in {json.dumps(video)}')
             continue
+
         if 'url_list' not in addr:
             log_error(f'No `url_list` in {json.dumps(addr)}')
             continue
@@ -72,6 +77,48 @@ def _get_douyin_video_url(share_url):
         url_list = addr['url_list']
         for url in url_list:
             urls.append(url)
+
+    return _make_success_result(urls)
+
+
+def _get_video_real_url_by_id_v2(video_id):
+    log_debug(f'video_id: {video_id}')
+    api_url = f'https://www.iesdouyin.com/aweme/v1/web/aweme/detail/?aweme_id={video_id}'
+    log_debug(f'api_url: {api_url}')
+    response = requests.get(url=api_url, headers=headers)
+    text = response.content.decode('utf-8')
+    log_debug(f'response: {text}')
+    response = json.loads(text)
+    if 'aweme_detail' not in response:
+        log_error('No `aweme_detail` in response')
+        return _make_fail_result('Bad response.')
+    aweme_detail = response['aweme_detail']
+    if len(aweme_detail) == 0:
+        log_error('No element in response.aweme_detail')
+        return _make_fail_result('Bad response.')
+
+    urls = []
+
+    if 'video' not in aweme_detail:
+        log_error(f'No `video` in {json.dumps(aweme_detail)}')
+        return _make_success_result(urls)
+    video = aweme_detail['video']
+
+    if 'download_addr' in video:
+        addr = video['download_addr']
+    elif 'play_addr' in video:
+        addr = video['play_addr']
+    else:
+        log_error(f'No `download_addr` or `play_addr` in {json.dumps(video)}')
+        return _make_success_result(urls)
+
+    if 'url_list' not in addr:
+        log_error(f'No `url_list` in {json.dumps(addr)}')
+        return _make_success_result(urls)
+
+    url_list = addr['url_list']
+    for url in url_list:
+        urls.append(url)
 
     return _make_success_result(urls)
 
@@ -103,13 +150,26 @@ class Resquest(BaseHTTPRequestHandler):
             http_status = 400
             response = _make_fail_result(f"Bad uri: {self.path}")
         else:
+            share_url = query['url']
+            video_id = ''
             try:
-                response = _get_douyin_video_url(query['url'])
+                video_id = _get_video_id(share_url)
             except Exception as ex:
                 http_status = 500
                 response = _make_fail_result(
-                    f'Unable to get the download url of douyin video, because: {ex}'
-                )
+                    f'Unable to get video id from share url, because: {ex}')
+
+            try:
+                response = _get_video_real_url_by_id_v1(video_id)
+            except:
+                try:
+                    response = _get_video_real_url_by_id_v2(video_id)
+                except Exception as ex:
+                    http_status = 500
+                    response = _make_fail_result(
+                        f'Unable to get the download url of douyin video, because: {ex}'
+                    )
+
         log_debug(f'response: {response}')
 
         self.send_response(http_status)
