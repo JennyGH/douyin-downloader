00bf1405b47aec83fded6b8af7776f4562f46826
JennyGH <674886730@qq.com>
2023-01-21 11:29:00 +0800

    还原

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --restart unless-stopped --name=douyin-downloader -v /var/services/homes/dbj/docker/douyin-downloader:/ --dns 202.96.128.86 -p 8081:8080 -d douyin-downloader
+docker run --restart unless-stopped --name=douyin-downloader -v /var/services/homes/dbj/docker/douyin-downloader/cache:/cache --dns 202.96.128.86 -p 8081:8080 -d douyin-downloader
